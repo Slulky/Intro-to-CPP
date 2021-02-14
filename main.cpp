c7059898f916a302bf10d5662be0b208656f5fe8
Sam Lulky <samflulky@gmail.com>
2021-02-14 22:39:29 +0200

    quadratic formula

--- main.cpp
+++ main.cpp
@@ -6,7 +6,12 @@ using namespace std;
 int main() {
 	int a, b, c;
 
-	cin >>
+	cin >> a >> b >> c;
 
-		return 0;
+	double discrim = sqrt(b * b - 4 * a * c);
+
+	cout << ((-b + discrim) / 2 * a * c) << endl;
+	cout << ((-b - discrim) / 2 * a * c) << endl;
+
+	return 0;
 }
